290f28e54515f1cc7bbb09cd30576bd94fad0de9
MrcSossy <scartidirobe@gmail.com>
2018-05-23 15:20:37 +0200

    Prima di ricostruire il progetto senza generatore

--- progetto/app.js
+++ progetto/app.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const fs = require('fs');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,16 +11,18 @@ var param = require('./routes/param');
 
 var app = express();
 
+/*----------------------------------------------------------------------------*/
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('view', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pages')));
+
+/*----------------------------------------------------------------------------*/
 
 app.use('/', routes);
 app.use('/users', users);
--- progetto/routes/cool.js
+++ progetto/routes/cool.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('you are so cool');
+router.get('/:nome', function(req, res) {
+  res.send('you are so cool '+req.params.nome);
 });
 
 module.exports = router;
