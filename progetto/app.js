41b97595a415c3a09f005f2f81b77bda1208f696
Mirco Santosuosso <scartidirobe@gmail.com>
2018-06-04 13:09:31 +0200

    Refactory finito
    R. lato server da rivedere in un secondo momento, per ora va tutto (vista e inserimento)

--- progetto/app.js
+++ progetto/app.js
@@ -1,7 +1,10 @@
 /*----------------------------------------------------------------------------*/ // Inizializzazione
 
 const express = require('express'); // Implementa Express
-const request = require('request'); // Implementa le chiamate
+const requ = require('./moduli/request'); // Implementa le chiamate personalizzate
+const request = require('request');
+const col = require('./moduli/colori');
+
 const bodyParser = require('body-parser');
 const parseUrlencoded = bodyParser.urlencoded({ extended: false });
 const app = express();
@@ -9,55 +12,32 @@ app.use(express.static('pages'));
 
 /*----------------------------------------------------------------------------*/ // Variabili
 
-/* Variabili colori */
-Black = "\x1b[30m"; Red = "\x1b[31m"; Green = "\x1b[32m"; Yellow = "\x1b[33m"; Blue = "\x1b[34m"; Magenta = "\x1b[35m"; Cyan = "\x1b[36m"; White = "\x1b[37m";
-
 /* Variabili statiche utili */
 const port = 8080; // Porta di ascolto
-const head = {Id:"ID", Titolo:"TITOLO", Status:"STATUS", Descrizione:"DESCRIZIONE"};
-const home = "<br><a = href='http://localhost:" + port + "/'>Clicca qui per tornare alla home</a>";
+exports.port = port;
+
+/* Variabili URL */
+const host = "http://stage.gnet.it/"; // Inizio URL
+const path = "rest/api/latest/"; // Metà dell'URL
+const base = host + path; // Tutta la prima parte dell'URL
+const srcPro = "search?jql=project=";
+const sort = "+order+by+summary"; // Ordina per titolo nella richiesta
+const issueUrl = base + "issue";
 
 /* Variabili autenticazione */
 const username = 'mrcsossy';
 const password = 'Stage.2018';
 const auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64'); // Richiesta basic
 
-/* Variabili URL */
-const host = "http://stage.gnet.it/"; // Inizio URL
-const path = "rest/api/latest/"; // Metà dell'URL
-const base = host + path; // Tutta la prima parte dell'URL
-const issueUrl = base + "issue";
-const srcPro = "search?jql=project=";
-const sort = "+order+by+summary"; // Ordina per titolo nella richiesta
-const fields = "fields=id,summary,description,status"; // Solo i campi interessati
-
 /*----------------------------------------------------------------------------*/ // Intercettazione richieste client
 
 app.get('/', (req, res) => res.render('index')); // Fornisce l'index
 
-app.get('/get', (req, res) => { // Richiesta di tutte le issues - FUNZIONANTE
-    neutral("Inizio richiesta di lettura");
+app.get('/get', (req, res) => requ.getAll(res)); // Richiesta di tutte le issues - FUNZIONANTE
+app.get('/get/:numero', (req, res) => req.getIssue(res, requ.params.numero)); // Richiesta di tutte le issues - FUNZIONANTE
 
-    request(
-        {
-            url: base + srcPro + "TODO" + sort + "&" + fields,
-            method: 'GET',
-            headers: {
-                'Authorization': auth
-            }
-        }, function(err, obj, body) {
-            if (err) {
-                error(err);
-            }
-            else {
-                good(out(res, body));
-            }
-        }
-    )
-});
-
-app.post('/create', parseUrlencoded, (req, res) => { // Creazione di una issue
-    neutral("Inizio richiesta di creazione di una issue");
+app.post('/create', parseUrlencoded, (req, res) => {
+    col.w("Inizio richiesta di creazione di una issue");
 
     request(
         {
@@ -80,99 +60,18 @@ app.post('/create', parseUrlencoded, (req, res) => { // Creazione di una issue
             }
         }, function(err, res, body) {
             if (err) {
-                error(err);
-            }
-            else {
-                good();
+                out.err(err);
             }
         }
     )
-});
+}); // Creazione di una issue
 
 app.get('/:error', (req, res) => {
     res.send("<h1>Il link " + req.params.error + " non è valido - error 404</h1>" + home);
-    red("Qualcuno ha ricevuto 404 all'indirizzo " + req.params.error);
+    col.r("Qualcuno ha ricevuto 404 all'indirizzo " + req.params.error);
 });
-
-/*----------------------------------------------------------------------------*/ // Funzioni varie
-
-/* Funzioni di output */
-function good(callback) {
-    green("Richiesta andata a buon fine\n");
-    callback;
-}
-function error(err) {
-    res.send("Siamo spiacenti ma si è verificato un errore" + home);
-    red("Richiesta fallita; " + err + "\n");
-}
-function out(res, body) {
-    var got = JSON.parse(body);
-
-    function extract(i) {
-        if(got.issues[i].fields.description === null) got.issues[i].fields.description = 'Nessuna descrizione';
-        var issue = {
-            'key': got.issues[i].key,
-            'summary': got.issues[i].fields.summary,
-            'status': got.issues[i].fields.status.statusCategory.name,
-            'description': got.issues[i].fields.description
-        };
-        return issue;
-    }
-
-    function issues() {
-        var issues = [];
-        for (var i in got.issues)
-            issues.push(extract(i));
-        return issues;
-    }
-
-    res.send(tabelize(issues()));
-}
-
-/* Funzioni per il log colorato */
-function green(str) {
-    console.log(Green + str + Red);
-}
-function red(str) {
-    console.log(Red + str);
-}
-function neutral(str) {
-    console.log(White + str + Red);
-}
-
-/* Funzioni per la paginazione */
-function riga(id, obj, isHead) {
-    if (isHead === undefined) isHead = false;
-    if (id === undefined) id = "";
-    var out = "";
-
-    if(isHead) out += "<thead><tr class='w3-light-grey fix'>";
-    else out += "<tr id='" + id + "'onclick='pop(" + id + ")'>";
-
-    for (var i in obj) {
-        out +=  "<td class='" + i + "'>\
-        " + obj[i] + "\
-        </td>";
-    }
-    out += "</tr>";
-    if(isHead) out += "</thead>";
-
-    return out;
-}
-function tabelize(obj) {
-    var out = "";
-
-    out += riga(i, head, true);
-
-    out += "<tbody>";
-    for (var i in obj)
-        out += riga(i, obj[i]);
-    out += "</tbody>";
-
-    return out;
-}
 
 /*----------------------------------------------------------------------------*/ // Fine
 
 /* Scrive la stringa se va tutto bene */
-app.listen(port, green('\n[ DONE ] Checks complete - server running, listening on port ' + port + '!\n'));
+app.listen(port, col.g('\n[ DONE ] Checks complete - server running, listening on port ' + port + '!\n'));
--- progetto/pages/script.js
+++ progetto/pages/script.js
@@ -1,46 +1,42 @@
 $(() => $("#out").load("/get")); // Al caricamento della pagina manda la richiesta al server
 
+var isDetails = true;
+
 function pop(id) {
-    $("#block").toggle();
-    if(id === undefined)
-        $("#details").toggle(500);
-    else
-        $("#crea").toggle(500);
-
-    if (id) {
-
+    if (id === undefined && isDetails) {
+        $(".blockC").toggle();
+        $(".create").toggle(500);
+    }
+    else {
+        isDetails = !isDetails;
+        $(".blockD").toggle();
+        $(".details").toggle(500);
     }
 }
 
 function undo() {
     pop();
-    $("#form")[0].reset();
+    $("form")[0].reset();
 }
 
 function create() {
     var titolo = $("#titolo").val();
-    var status = $("#status").val();
-    var descrizione = $("textarea").val();
+    var descrizione = $("textarea#descrizione").val();
+    var commento = $("textarea#commento").val();
 
-    if (titolo.length > 0 && status.length > 0) {
-        if (!descrizione.length > 0) {
+    if (titolo.length > 0) {
+        if (!descrizione.length > 0)
             descrizione = "";
-        }
-        $.post("/create", {"tit": titolo, "sta":status, "des":descrizione});
+        if (!commento.length > 0)
+            commento = "";
+        $.post("/create", {"tit": titolo, "des":descrizione, "comm":commento});
         undo();
-        setTimeout(()=>$("#out").load("/get"), 100);
     }
-    else {
-        if (!titolo.length > 0) {
-            alert("Manca il titolo");
-        }
-        if (!status.length > 0) {
-            alert("Manca lo status");
-        }
-    }
+    else
+        alert("Manca il titolo");
 }
 
-setTimeout(()=>$("#out").load("/get"), 1000);
+setInterval(() => $("#out").load("/get"), 2000); // Aggiorna la tabella ogni 2 secondi
 
 // // Disable #x
 // $( "#x" ).prop( "disabled", true );
